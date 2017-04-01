0428389217b62aec00525e6e8e690af9c32d3b6a
Theo Goulart <theo_goulart@hotmail.com>
2017-04-01 17:39:46 +0000

    add multer to get filesizes and send it in a JSON response

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 var express = require("express")
-var path = require("path")
+var multer  = require("multer")
+var path    = require("path")
+
 var app = express()
+var upload = multer({storage: multer.memoryStorage()})
 
 app.use(express.static('view'))
+
+app.post('/', upload.single("file"), function(req, res){
+    
+    var result = {}
+    result.size = req.file.size
+    
+    res.status(200).send(result)
+})
 
 app.get('/',function(req, res){
     res.sendFile(path.join(__dirname+'/view/index.html'))
